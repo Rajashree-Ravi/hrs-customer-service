3e189a56ae08a9a49ac0aa99be6c0eda3a8dbfc7
Rajashree Ravi <rajashreeravi09@gmail.com>
2023-10-26 20:35:25 +0530

    Zuul proxy working

---
+++ src/test/java/com/hrs/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrs.customerservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CustomerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
