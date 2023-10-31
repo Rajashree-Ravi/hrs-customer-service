e5229976390850f333f9379ee911cf5fa77dd14d
Rajashree Ravi <rajashreeravi09@gmail.com>
2023-10-31 11:55:02 +0530

    Updated security config for gateway

--- src/main/java/com/hrs/customerservice/configurations/WebSecurityConfig.java
+++ src/main/java/com/hrs/customerservice/configurations/WebSecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,8 +15,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import com.hrs.customerservice.services.CustomerAuthenticationService;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -26,14 +22,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+		http.authorizeRequests().antMatchers("/api/**").permitAll().antMatchers("/**").authenticated().and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
 	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
-				.requestMatchers(new AntPathRequestMatcher("/api/test"));
+		web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
 	}
 
 	@Override
